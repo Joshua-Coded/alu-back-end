cb3d9cb506814fcca3c3bd6d2867a6f62a57e523
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-05 16:34:27 +0200

    task 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,15 +14,12 @@ if __name__ == "__main__":
 
     user_response = requests.get(user_data_url)
     todo_response = requests.get(todo_url)
-
     # if todo_response.status_code & user_response.status_code == 200:
     todos = todo_response.json()
     users = user_response.json()
-
     for user in users:
         if user.get("id") == employee_Id:
             employee_name = user.get("name")
-
     # filter completed tasks
     done = []
     total = 0
@@ -33,7 +30,6 @@ if __name__ == "__main__":
             if todo.get("completed"):
                 completed += 1
                 done.append(todo.get("title"))
-
     # Display the progress information
     print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
     for _ in done:
