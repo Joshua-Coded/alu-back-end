7dcef162c3fe4edb9ab0940f9bb278b411868d5e
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-11-05 16:15:48 +0200

    task 1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+getting data from the api
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_Id = int(sys.argv[1])
+
+    todo_url = "https://jsonplaceholder.typicode.com/todos"
+    user_data_url = "https://jsonplaceholder.typicode.com/users"
+
+    user_response = requests.get(user_data_url)
+    todo_response = requests.get(todo_url)
+
+    # if todo_response.status_code & user_response.status_code == 200:
+    todos = todo_response.json()
+    users = user_response.json()
+
+    for user in users:
+        if user.get("id") == employee_Id:
+            employee_name = user.get("name")
+
+    # filter completed tasks
+    done = []
+    total = 0
+    completed = 0
+    for todo in todos:
+        if todo.get("userId") == employee_Id:
+            total += 1
+            if todo.get("completed"):
+                completed += 1
+                done.append(todo.get("title"))
+
+    # Display the progress information
+    print(f"Employee {employee_name} is done with tasks({completed}/{total}):")
+    for _ in done:
+        print(f"\t {_}")
